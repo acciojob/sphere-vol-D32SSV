24a508a68ed5f5328542b37cf67ad3ab06b4403d
Arun Prakash Pandey <131158288+D32SSV@users.noreply.github.com>
2023-08-28 10:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
     //Write your code here
-  
+	let rad = parseFloat(document.getElementById("radius").value);
+
+	let vol = (Math.PI *rad*rad*rad*4)/3 ;
+
+	let update = document.getElementById("volume") ;
+	update.value = vol ;  
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = function (event) {
+            event.preventDefault(); // Prevent form submission
+            volume_sphere();
+        };
+
+
